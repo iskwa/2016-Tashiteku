889bc6b9ad0e23ec76e3659f16ff22c19e6e6881
Junichi Kobayashi <junichi.kobayashi@esm.co.jp>
2016-06-28 19:19:06 +0900

    Enable to view html

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!\n');
-}).listen(8080);
+var fs = require('fs');
+var server = http.createServer();
+server.on('request', function (req, res) {
+  fs.readFile('./javascript/makePostit/test.html', 'utf-8', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+});
+server.listen(8080);
+console.log('server listening...');
 
